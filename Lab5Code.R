7bc2c1a8dc0a3bb83d700757f518dd5a09b6b847
averyqjohnson <aqjohnson@colgate.edu>
2025-02-18 15:10:06 -0500

    Step 1

---
+++ Lab5Code.R
@@ -0,0 +1,9 @@
+##############################################################################
+# Lab 5
+# Avery Johnson
+##############################################################################
+library("tidyverse")
+
+essentia_data_allentown <- read_csv("data/essentia.data.allentown.csv")
+essentia_data <- read_csv("data/essentia.data.csv")
+view(essentia_data_allentown)         
